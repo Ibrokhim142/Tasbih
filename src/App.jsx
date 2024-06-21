29136434c94cae89afa17964eae0e27af28dad36
Macbook Air <macbookair@MacBook-Air-Macbook.local>
2024-06-21 23:52:30 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Tasbih from './components/Tasbih/Tasbih.jsx';
+
+function App() {
+    return (
+        <div className="container">
+            <div className="pages">
+            <Tasbih/>
+            </div>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Tasbih/Tasbih.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect, useRef } from 'react';
+import './Tasbih.css';
+const Tasbih = () => {
+    const [count, setCount] = useState(0);
+    const audioRef = useRef(null);
+
+    
+
+    const playSound = () => {
+        setCount((prevCount) => (prevCount < 99 ? prevCount + 1 : 1));
+    };
+    useEffect(() => {
+      if (count === 33 || count === 66 || count === 99) {
+          audioRef.current.play();
+      }
+  }, [count]);
+
+
+    return (
+        <div className='tasbih'>
+            <h1>Tasbih</h1>
+            <p>Sanoq: {count}</p>
+            <button onClick={playSound}>Tasbihni bosing</button>
+            <audio ref={audioRef} src="..//public/Bismillah.mp3" />
+        </div>
+    );
+};
+
+export default Tasbih;
